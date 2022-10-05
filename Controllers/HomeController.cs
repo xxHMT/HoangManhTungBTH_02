1c128296f491d197050f3371af333736dec6f239
xxHMT <algoroku@gmail.com>
2022-10-05 16:20:04 +0700

    05_10

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using HoangManhTungBTH_02.Models;
+
+namespace HoangManhTungBTH_02.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ Models/Student.cs
@@ -0,0 +1,8 @@
+namespace HoangManhTungBTH_02.Models 
+{
+    public class Student
+    {
+        public string StudentID { get; set; }
+        public string StudentName { get; set; }
+    }
+}
